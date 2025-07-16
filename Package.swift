6f21ec461c7f24c27aee72a0aedd14918b9eddbf
Louis Jordan <louisjoecodes@gmail.com>
2025-07-16 18:03:09 +0100

    base

--- Package.swift
+++ Package.swift
@@ -1,33 +1,47 @@
-// swift-tools-version: 6.0
+// swift-tools-version:6.0
+// (Xcode16.0+)
+
 import PackageDescription
 
 let package = Package(
-    name: "ElevenLabsSwift",
+    name: "ElevenLabs",
     platforms: [
-        .iOS(.v16),
+        .iOS(.v13),
         .macOS(.v10_15),
+        .macCatalyst(.v14),
+        .visionOS(.v1),
+        .tvOS(.v17),
     ],
     products: [
         .library(
-            name: "ElevenLabsSwift",
-            targets: ["ElevenLabsSwift"]
+            name: "ElevenLabs",
+            targets: ["ElevenLabs"]
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/livekit/client-sdk-swift.git", from: "2.0.0"),
-        .package(url: "https://github.com/devicekit/DeviceKit.git", from: "5.6.0"),
+        .package(url: "https://github.com/livekit/client-sdk-swift.git", from: "2.6.0"),
+        .package(url: "https://github.com/apple/swift-log.git", from: "1.6.3"),
+        .package(url: "https://github.com/apple/swift-async-algorithms.git", from: "1.0.3"),
     ],
     targets: [
         .target(
-            name: "ElevenLabsSwift",
+            name: "ElevenLabs",
             dependencies: [
                 .product(name: "LiveKit", package: "client-sdk-swift"),
-                "DeviceKit",
+                .product(name: "Logging", package: "swift-log"),
+                .product(name: "AsyncAlgorithms", package: "swift-async-algorithms"),
+            ],
+            resources: [
+                .process("PrivacyInfo.xcprivacy"),
             ]
         ),
         .testTarget(
-            name: "ElevenLabsSwiftTests",
-            dependencies: ["ElevenLabsSwift"]
+            name: "ElevenLabsTests",
+            dependencies: ["ElevenLabs"]
         ),
+    ],
+    swiftLanguageModes: [
+        .v5,
+        .v6,
     ]
 )
