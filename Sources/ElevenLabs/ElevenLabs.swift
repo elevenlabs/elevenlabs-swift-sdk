815d0ae6495c2465c067a81d8b45cc2eb7163168
Louis J. <132601011+louisjoecodes@users.noreply.github.com>
2025-10-20 13:44:27 +0100

    chore: bump version 2.0.15 (#119)

--- Sources/ElevenLabs/ElevenLabs.swift
+++ Sources/ElevenLabs/ElevenLabs.swift
@@ -20,7 +20,7 @@ import LiveKit
 public enum ElevenLabs {
     // MARK: - Version
 
-    public static let version = "2.0.14"
+    public static let version = "2.0.15"
 
     // MARK: - Configuration
 
