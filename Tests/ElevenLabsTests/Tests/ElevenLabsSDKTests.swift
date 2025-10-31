d3f60c349c62e566189dbe8eddeb6083190fa477
Paul Asjes <paul.asjes@elevenlabs.io>
2025-10-31 14:25:49 +0100

    Version bump

--- Tests/ElevenLabsTests/Tests/ElevenLabsSDKTests.swift
+++ Tests/ElevenLabsTests/Tests/ElevenLabsSDKTests.swift
@@ -4,7 +4,7 @@ import XCTest
 
 final class ElevenLabsSDKTests: XCTestCase {
     func testSDKVersionExists() {
-        XCTAssertEqual(ElevenLabs.version, "2.0.15")
+        XCTAssertEqual(ElevenLabs.version, "2.0.16")
         XCTAssertFalse(ElevenLabs.version.isEmpty)
     }
 
