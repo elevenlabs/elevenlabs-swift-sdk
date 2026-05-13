0db69c21dc526319a891ddfa4ad768e5dba6bb35
Paul Asjes <paul.asjes@elevenlabs.io>
2026-05-13 10:06:37 +0200

    Bump visionOS version and patch (#178)
    * bump visionOS version and patch
    
    * correct upgrade path

---
+++ Package@swift-6.2.swift
@@ -0,0 +1,53 @@
+// swift-tools-version:6.2
+// Swift 6.2+ (Xcode 26.0+)
+
+import PackageDescription
+
+let package = Package(
+    name: "ElevenLabs",
+    platforms: [
+        .iOS(.v13),
+        .macOS(.v10_15),
+        .macCatalyst(.v14),
+        .visionOS(.v26),
+        .tvOS(.v17)
+    ],
+    products: [
+        .library(
+            name: "ElevenLabs",
+            targets: ["ElevenLabs"]
+        )
+    ],
+    dependencies: [
+        .package(url: "https://github.com/livekit/client-sdk-swift.git", from: "2.10.0")
+    ],
+    targets: [
+        .target(
+            name: "ElevenLabs",
+            dependencies: [
+                .product(name: "LiveKit", package: "client-sdk-swift")
+            ],
+            // TODO: Re-enable StrictConcurrency once LiveKit depends on a JWTKit
+            // release where Sendable annotations are available (4.13.x patch or 5.x).
+            // Tracking: https://github.com/livekit/client-sdk-swift/issues/846
+            exclude: [
+                "Internal/Protocol/schemas/agent.asyncapi.yaml"
+            ],
+            resources: [
+                .process("PrivacyInfo.xcprivacy")
+            ]
+        ),
+        .testTarget(
+            name: "ElevenLabsTests",
+            dependencies: [
+                "ElevenLabs",
+                .product(name: "LiveKit", package: "client-sdk-swift")
+            ],
+            resources: [
+                .copy("Resources/silence.mp3"),
+                .copy("Resources/spoken-audio.mp3")
+            ]
+        )
+    ],
+    swiftLanguageModes: [.v5]
+)
