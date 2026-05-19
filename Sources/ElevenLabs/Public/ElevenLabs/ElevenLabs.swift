8d177963354c1b84d9d9045a34651a6731857180
Renal Khabibulin <1023593+renal128@users.noreply.github.com>
2026-05-19 11:19:48 +0100

    chore: bump version to 3.2.0

--- Sources/ElevenLabs/Public/ElevenLabs/ElevenLabs.swift
+++ Sources/ElevenLabs/Public/ElevenLabs/ElevenLabs.swift
@@ -20,7 +20,7 @@ import LiveKit
 public enum ElevenLabs {
     // MARK: - Version
 
-    public static let version = "3.1.4"
+    public static let version = "3.2.0"
 
     // MARK: - Configuration
 
